01647ec1a30db93051185b59e54056ef75b9ce9b
Annika Lang <Lang-annika@t-online.de>
2019-11-12 23:09:28 +0100

    finished checkWin funtion

--- script.js
+++ script.js
@@ -41,8 +41,12 @@ function checkWin(board, player) {
     (e === player)) ? a.concat(i) : a, [];
   let gameWon = null;
   for (let [index, win] of winCombos.entries()) {
-
+    if (win.every(elem => plays.indexOf(elem > -1)) {
+      gameWon = {index: index, player: player};
+      break;
+    })
   }
+  return gameWon;
 }
 
 
